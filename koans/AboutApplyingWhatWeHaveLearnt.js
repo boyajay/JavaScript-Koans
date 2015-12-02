05e15f0d85b869f758668c7423da3c87985a394d
Boya Jiang <jiang.boya@yahoo.com>
2015-12-02 16:46:18 -0500

    Section 1 Requirements Complete

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -30,16 +30,19 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
       var productsICanEat = [];
-
+      productsICanEat = _(products).filter(function(food){
+        return _(food.ingredients).all(function(ingr) {return ingr != "mushrooms"}) && !food.containsNuts;  
+      }); 
       /* solve using filter() & all() / any() */
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      expect(productsICanEat.length).toBe(1);
   });
+
 
   /*********************************************************************************/
 
@@ -56,7 +59,10 @@ describe("About Applying What We Have Learnt", function() {
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+
+    var sum = _.chain(_.range(1000))
+      .reduce(function(a, b){(if (b%3 === 0 || b%5 === 0) { return a+b} else return a;)})
+      .value();
 
     expect(233168).toBe(FILL_ME_IN);
   });
